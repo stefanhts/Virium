4ed676014c08697d74cf2f7dea95ae3404e5e12e
stefanhts <stefan.heller6341@gmail.com>
2017-04-12 21:50:07 -0400

    Update ButtVI.java

--- ButtVI.java
+++ ButtVI.java
@@ -1,28 +1,15 @@
 
-
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-public class ButtVI {
+public class mainFunction {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		// TODO Auto-generated method stub
-		// default icon, custom title
-		Object[] options = { "QUIT", "GET HELP" };
-		int n = JOptionPane.showOptionDialog(frame, "A VIRUS HAS BEEN DETECTED!!", "ATENTION!",
-				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options,options[0]);
-		frame.addWindowListener(new java.awt.event.WindowAdapter() {
-		    @Override
-		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-		        if (JOptionPane.showConfirmDialog(frame, 
-		            "Are you sure to close this window?", "Really Closing?", 
-		            JOptionPane.YES_NO_OPTION,
-		            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
-		            System.exit(0);
-		        }
-		    }
-		});
+		
+		
+		createPopUp window = new createPopUp("ERROR");
+		window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+	
+
 	}
 
 }
